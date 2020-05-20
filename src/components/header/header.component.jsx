ac0993ddb2e4b193eab3bc238f4c5cf072cd16e3
rramos85 <rodrigues.ramos@gmail.com>
2020-05-20 16:28:19 -0300

    Segundo commit - adicionando o componente backpack-dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,18 +5,35 @@ import BackpackIcon from '../backpack-icon/backpack-icon.component'
 import BackpackDropdown from '../backpack-dropdown/backpack-dropdown.component'
 
 
-const Header = () => (
-    <div className="header">
-    <Link className='logo-container' to='/'>
-      <h1>Mercado Noturno</h1>
-      <h1>Equipe Rocket</h1>
-    </Link>
-    <div className='options'>
-        <Link className='option' to='/search'>Pesquisar</Link>
-        <Link className='option' to='/backpack'>Mochila</Link>
-        <Link className='option' to='/'> <BackpackIcon /> </Link>       
-    </div> 
-    <BackpackDropdown />
-  </div>
+class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isEnabled: false
+    }
+  }
+
+  toggleEnabled = () => (
+    this.setState({isEnabled : !this.state.isEnabled})
   )
+  render() {
+    return(
+      <div className="header">
+        <Link className='logo-container' to='/'>
+          <h1>Mercado Noturno</h1>
+          <h1>Equipe Rocket</h1>
+        </Link>
+        <div className='options'>
+          <Link className='option' to='/search'>Pesquisar</Link>
+          <Link className='option' to='/backpack'>Mochila</Link>
+          <Link className='option'  onClick={this.toggleEnabled} > <BackpackIcon /> </Link>
+        </div>
+        {
+          this.state.isEnabled && <BackpackDropdown />
+        } 
+      </div>
+    )
+  }
+    
+}
 export default Header
